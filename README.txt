82323fa64e1baefa5a960fccef1e199b0a942546
vladislav <vladskorikov@gmail.com>
2021-01-08 15:23:26 +0400

    setup

---
+++ README.txt
@@ -0,0 +1,1 @@
+Web server for my site алтай64.рф
--- altai64/webserver.py
+++ altai64/webserver.py
@@ -18,4 +18,4 @@ def callback():
     abort(401)
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=9090)
+    app.run(host='127.0.0.1', port=9090
